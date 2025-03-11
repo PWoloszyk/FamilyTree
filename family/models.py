347c94268fb5ccc612d8f52cebfbb5b577189225
PWoloszyk <piotrwoloszyk@outlook.com>
2025-03-11 16:03:45 +0100

    add listview

--- family/models.py
+++ family/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    death_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    # image = models.ImageField(upload_to="images/", null=True, blank=True)
+    father = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="father_of",
+    )
+    mother = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="mother_of",
+    )
+    partners = models.ManyToManyField(
+        "self", blank=True, symmetrical=True, related_name="partners"
+    )
+    root = models.BooleanField("root", default=False)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- family/views.py
+++ family/views.py
@@ -1,3 +1,24 @@
+from django.views.generic import ListView
+from .models import Person
 from django.shortcuts import render
 
-# Create your views here.
+
+class FamilyList(ListView):
+    def get_queryset(*args, **kwargs):
+        return Person.objects.all()
+
+    def get_context_data(self, family_list=None, **kwargs):
+        queryset = family_list or self.object_list
+        return super().get_context_data(family_list=queryset, **kwargs)
+
+
+def family_tree_view(request):
+    return render(request, "homepage.html")
+
+
+class PersonDetailView(ListView):
+    model = Person
+
+    def get_context_data(self, **kwargs):
+        person = Person.objects.get(pk=self.kwargs["pk"])
+        return super().get_context_data(person=person, **kwargs)
